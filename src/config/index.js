dd8e3f229ffce0d0d0a95213877a5d9b3a5cfe6c
ArtNaja <190624439+ArtNaja@users.noreply.github.com>
2026-03-10 04:51:29 +0700

    fix: use BASE_URL for image paths

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ export const LOCATIONS = [
     id: 1,
     sensorId: 'ESP32_01',
     gid: '1098888062',
-    image: '/img/Library.jpg',
+    image: `${import.meta.env.BASE_URL}img/Library.jpg`,
     name: 'อาคารบรรณสาร (Library)',
     fallback:
       'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?q=80&w=1000&auto=format&fit=crop',
@@ -17,7 +17,7 @@ export const LOCATIONS = [
     id: 2,
     sensorId: 'ESP32_02',
     gid: '1804783139',
-    image: '/img/LearningBuilding_1.jpg',
+    image: `${import.meta.env.BASE_URL}img/LearningBuilding_1.jpg`,
     name: 'อาคารเรียนรวม 1 (Learning Building 1)',
     fallback:
       'https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1000&auto=format&fit=crop',
